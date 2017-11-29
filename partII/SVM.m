eeeacbee88d35fc9402072e02508adf37bc1c5bc
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2017-11-29 11:56:55 +0000

    SVM

--- partII/SVM.m
+++ partII/SVM.m
@@ -7,9 +7,30 @@ function SVM()
     load('X.mat'); 
     load('l.mat');
     load('X_test.mat');
-    load('l_test.mat');    
+    load('l_test.mat');
     
-    accuracy = 0; % to be calculated
+    
+    N = length(l);
+    S_t = (1/N) * (X*X');
+    
+    aK  = diag(l) * X';
+    K_y = aK / S_t * aK';
+    
+    a_l = zeros(N, 1);
+    a_u = 1*ones(N,1);
+    
+    f = -ones(1,N);
+    
+    c   = 0;
+    A_e = l';
+    c_e = 0;
+    A   = zeros(1, N);
+    
+    alpha = quadprog(K_y,f,A,c,A_e,c_e,a_l,a_u);
+    
+    res_labels = sign((S_t \ X * (alpha .* l))' * X_test)';
+    
+    accuracy = sum(res_labels == l_test) / length(res_labels);
     fprintf('Accuracy on the test set is %3.2f\n', accuracy);
 
 end
