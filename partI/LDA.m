3f2f03e5c4ba0c66e0f55dc2ac7b15b712a8e078
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2017-11-29 12:00:57 +0000

    LDA PCA code cleanup

--- partI/LDA.m
+++ partI/LDA.m
@@ -11,16 +11,12 @@ function W = LDA(data, labels)
     % array of all Ei matrices
     
     E_is = arrayfun(@(it) 1/it*ones(it), n_eis, 'UniformOutput', false);
-%     E_is = {};
-%     for it = n_eis'
-%         E_is = [E_is, 1/it * ones(it)];
-%     end
     
     M = blkdiag(E_is{:});
     
     i_minus_m = (eye(length(labels)) - M);
-    [V_w, Lambda_w] = eigs(i_minus_m * central_data * central_data' * i_minus_m, size(central_data, 1) - (size(unique(labels), 1) + 1));
-    U = central_data' * i_minus_m * V_w * inv(Lambda_w);
+    [V_w, Lambda_w] = eigs(i_minus_m * central_data * central_data' * i_minus_m, size(central_data, 1) - (size(unique_labels, 1) + 1));
+    U = central_data' * i_minus_m * V_w / Lambda_w;
     
     proj_data = (U'*central_data'*M)';
     Q = pca(proj_data);
--- partI/PCA.m
+++ partI/PCA.m
@@ -4,5 +4,7 @@ function U = PCA(data)
     covar = central_data * central_data';
     [V, D] = eigs(covar, size(central_data, 1)-1);
     U = central_data' * V * D^-0.5;
+
+%  % alternative solution using the Matlab built-in pca function
 %     U = pca(data);
 end
